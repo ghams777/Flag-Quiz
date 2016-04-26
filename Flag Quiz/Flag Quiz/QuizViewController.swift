b6178c43f0aefccf7d20ee12f527427ba1c21d0a
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-26 14:02:20 +0700

    QuizViewController Class, Properties

--- Flag Quiz/Flag Quiz/QuizViewController.swift
+++ Flag Quiz/Flag Quiz/QuizViewController.swift
@@ -15,6 +15,18 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var questionNumberLabel: UILabel!
     @IBOutlet var segmentedControl: [UISegmentedControl]!
     
+    
+    private let correctColor = UIColor(red: 0.0, green: 0.75, blue: 0.0, alpha: 1.0)
+    private let incorrectColor = UIColor.redColor()
+    
+    private var quizCountries: [String]! = nil // countries in quiz
+    private var enabledCountries: [String]! = nil // countries for guesses
+    
+    private var correntAnswer: String! = nil
+    private var correctGuesses = 0
+    private var totalGuesses = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
