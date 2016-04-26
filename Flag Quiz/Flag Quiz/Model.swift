c31723ad71d907ac0b9291e5b4a1ed498d71b073
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-26 10:44:35 +0700

    Model Class, ModelDelegate Protocol, Model Properties

--- Flag Quiz/Flag Quiz/Model.swift
+++ Flag Quiz/Flag Quiz/Model.swift
@@ -13,3 +13,30 @@ import Foundation
 protocol ModelDelegate {
     func settingChanged()
 }
+
+class Model {
+    
+    // keys for storing data in the app's NSUserDefaults
+    private let regionsKey = "FlagQuizKeyRegions"
+    private let guessesKey = "FlagQuizKeyGuesses"
+    
+    // reference to QuizViewController to notify it when settings change
+    private var delegate: ModelDelegate! = nil
+    
+    var numberOfGuesses = 4 // number of guesses to display
+    private var enabledRegions = [
+        "Africa": false,
+        "Asia": false,
+        "Europe": false,
+        "North_America": true,
+        "Oceania": false,
+        "South_America": false
+    ]
+    
+    // variables for maintaining quiz data
+    let numberOfQuestions = 10
+    private var allCountries: [String] = []
+    private var countriesInEnabledRegions: [String] = []
+    
+    
+}
