9168bd92831ddf1dfd82e478ba5cb6d1e7ba409d
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-26 14:13:17 +0700

    “Overridden UIViewController Method viewDidLoad, and Methods settingsChanged and resetQuiz”
    Excerpt From: Paul Deitel. “iOS 8 for Programmers: An App-Driven
    Approach with Swift (3rd Edition) (Deitel Developer Series).” iBooks.

--- Flag Quiz/Flag Quiz/QuizViewController.swift
+++ Flag Quiz/Flag Quiz/QuizViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, ModelDelegate {
 
     @IBOutlet weak var flagImageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questionNumberLabel: UILabel!
-    @IBOutlet var segmentedControl: [UISegmentedControl]!
+
+    @IBOutlet var segmentedControls: [UISegmentedControl]!
     
+    private var model: Model! // reference to the model object
     
     private let correctColor = UIColor(red: 0.0, green: 0.75, blue: 0.0, alpha: 1.0)
     private let incorrectColor = UIColor.redColor()
+    
     
     private var quizCountries: [String]! = nil // countries in quiz
     private var enabledCountries: [String]! = nil // countries for guesses
@@ -30,7 +33,34 @@ class QuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // create Model
+        model = Model(delegate: self)
+        settingChanged()
     }
+    
+    
+    // SettingsDelegate: reconfigures quiz when user changes settings: also called when app first loads
+    func settingChanged() {
+        enabledCountries = model.enabledRegionCountries
+        resetQuiz()
+    }
+    
+    
+    // start q new quiz
+    func resetQuiz() {
+        quizCountries = model.newQuizCountries() // countries in new quiz
+        correctGuesses = 0
+        totalGuesses = 0
+        
+        //display appropriate # of UISegmentedControls
+        for i in 0 ..< segmentedControls.count {
+            segmentedControls[i].hidden = (i < model.numberOfGuesses / 2) ? false : true
+        }
+        
+        nextQuestion() // display the first flag in quiz
+    }
+    
     
     @IBAction func submitGuess(sender: AnyObject) {
     }
